e065f518ef07eefcba007248c131a5bb4e409a09
Rabi Ul haq <rs2816868@gmail.com>
2024-04-02 23:06:53 +0500

    Todo_list_assigment
    This is Simple Todo List Assigment

---
+++ index.js
@@ -0,0 +1,21 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: " What do you Want to Add in Todo List ?"
+        },
+        {
+            name: "addMore",
+            type: "confirm ",
+            message: "Do You want to add more in Todo List ? ",
+        }
+    ]);
+    todos.push(addTask.todo);
+    condition = addTask.addMore;
+    console.log(todos);
+}
